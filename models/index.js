5cf34be0976f03fb3e62a6e9d691d47288ee778e
Kendrameda <gibsoken@gmail.com>
2023-01-11 21:33:48 -0600

    tried fixing code

--- models/index.js
+++ models/index.js
@@ -5,9 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsToMany(Category, {as: 'category_products'});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-Category.belongsToMany(Product, {as: 'product_products'});
+Category.belongsTo(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
